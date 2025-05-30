4d0178acb4cdd987c3acda6d1d472f791400131c
167557 <167557@5CD3441DMK>
2025-05-30 17:51:07 +0530

    from LAP

--- src/TakingScreenshots.java
+++ src/TakingScreenshots.java
@@ -10,14 +10,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class TakingScreenshots {
 
 	public static void main(String[] args) throws IOException {
-		System.setProperty("webdriver.chrome.driver",
-				"C://Users//allan//OneDrive//Documents//chromedriver-win64//chromedriver.exe");
+//		System.setProperty("webdriver.chrome.driver",
+//				"C://Users//allan//OneDrive//Documents//chromedriver-win64//chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.navigate().to("https://rahulshettyacademy.com/AutomationPractice/");
 		driver.manage().window().maximize();
 		File File = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 		FileUtils.copyFile(File, new File("C:\\Users\\allan\\OneDrive\\Pictures\\Screenshots\\Screenshot.jpeg"));
-		System.out.println("GIT check cheyunne");
+		//System.out.println("GIT check cheyunne");
 	}
 
 }
